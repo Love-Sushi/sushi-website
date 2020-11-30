721f6c19b88aeb515a836e4af5910ab886c117a8
Sherif Meimari <sherif.meimari@indigen.com>
2020-11-30 20:29:26 +0200

    fix: formatted code

--- release.config.js
+++ release.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  branches: "master",
-  repositoryUrl: "https://github.com/Love-Sushi/sushi-website",
+  branches: 'master',
+  repositoryUrl: 'https://github.com/Love-Sushi/sushi-website',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
-  ]
-}
-
-
-
-
+    '@semantic-release/github',
+  ],
+};
