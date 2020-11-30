723af78873f04462633ed11e04ab065e48c713fd
Sherif Meimari <sherif.meimari@indigen.com>
2020-11-30 20:25:16 +0200

    feat: added ci and semantic-release

---
+++ release.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/Love-Sushi/sushi-website",
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ]
+}
+
+
+
+
