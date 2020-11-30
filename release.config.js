bc04c279803270471ac8213a62504319ac0cf8f0
Sherif Meimari <sherif.meimari@indigen.com>
2020-11-30 21:27:35 +0200

    fix: fixed main branch name in release config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: 'master',
+  branches: 'main',
   repositoryUrl: 'https://github.com/Love-Sushi/sushi-website',
   plugins: [
     '@semantic-release/commit-analyzer',
